896fb417a6d8ed12f8ef6eb7237f8cf58feb3d28
Dustin Ingram <dustin.ingram@gmail.com>
2014-07-02 22:11:13 -0400

    Added template tools

---
+++ template_tools.py
@@ -0,0 +1,8 @@
+from portphilio_lib.models import *
+
+def get_category(config, category_name):
+    return Category.objects.get(slug=category_name, owner=config["HOST"].owner)
+
+def get_body(config):
+    return Body.objects.get(owner=config["HOST"].owner)
+
--- tools.py
+++ tools.py
@@ -100,10 +100,6 @@ def retrieve_image(image_name, user_name):
         return redirect("%s%s" % (url, image_name))
 
 
-def get_category(config, category_name):
-    return Category.objects.get(slug=category_name, owner=config["HOST"].owner)
-
-
 def get_host_by_hostname(hostname):
     try:
         return Host.objects.get(hostname=hostname)
