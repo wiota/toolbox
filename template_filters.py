d20b2082c4dc677a816726a63b41048ad95c36d4
Dustin Ingram <dustin.ingram@gmail.com>
2015-02-23 21:53:47 -0500

    Adding cachebuster as jinja filter

--- template_filters.py
+++ template_filters.py
@@ -1,6 +1,7 @@
 import re
 from time import strftime, localtime
 from jinja2 import escape
+from flask import current_app as app
 
 _paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
 
@@ -12,3 +13,7 @@ def format_money(value):
 
 def format_nl2br(value):
     return u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', '<br>\n') for p in _paragraph_re.split(escape(value)))
+
+def bust(url):
+    """ Adds a cache buster to the URL. """
+    return "%s?v=%s" % (url, app.config["HEROKU_RELEASE_NAME"])
