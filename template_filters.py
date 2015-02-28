6cd2b16b2c90380ad066afc190302333dcf39f2d
ryanhinkel <ryanhinkel@gmail.com>
2015-02-27 19:29:41 -0500

    Bug REALLY fixed this time.

--- template_filters.py
+++ template_filters.py
@@ -12,7 +12,7 @@ def format_money(value):
     return "$%.2f" % (value/100.0)
 
 def format_nl2br(value):
-    return u'\n\n'.join(u'<p>%s</p>' % p.replace(u'\n', u'<br>\n') for p in _paragraph_re.split(escape(value)))
+    return u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', u'<br>\n') for p in _paragraph_re.split(value))
 
 def bust(url):
     """ Adds a cache buster to the URL. """
